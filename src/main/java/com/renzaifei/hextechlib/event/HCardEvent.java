162e152ea200eff712f026621288d0c8416a2880
EvanHsieh0415 (school) <d11219121@takming.edu.tw>
2026-02-11 15:06:31 +0800

    feat: add event system

--- src/main/java/com/renzaifei/hextechlib/event/HCardEvent.java
+++ src/main/java/com/renzaifei/hextechlib/event/HCardEvent.java
@@ -1,4 +1,16 @@
 package com.renzaifei.hextechlib.event;
 
-public class HCardEvent {
+import com.renzaifei.hextechlib.card.HCard;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+
+public class HCardEvent extends PlayerEvent {
+    HCard card;
+
+    public HCardEvent(Player player, HCard card) {
+        super(player);
+        this.card = card;
+    }
+
+
 }
